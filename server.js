1bfbb908bd560501838d1c986bb1baed1c19ee46
Cynthia Chaves <cynthia_chaves@aol.com>
2024-10-07 14:57:36 -0400

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
 
 app.get('/', async (req, res) => {
     res.render('index.ejs');
